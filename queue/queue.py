c407cc7d01ab882d80b00d133241b3ddebae7fca
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-08 15:58:48 -0500

    queue done

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,25 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.size !=0:
+             self.size -= 1
+             return self.storage.remove_head()
+
+# question 3 answer 
+# In array, Insertion and Deletion operation takes more time, as the memory locations are consecutive and fixed.
+# In case of linked list, a new element is stored at the first free and available memory location,
+#  with only a single overhead step of storing the address of memory location in the previous node of linked list.
