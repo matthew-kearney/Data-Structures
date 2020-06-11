7fb20bd85cc6dbb593755385262d20bb936f4b5a
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-11 15:38:50 -0500

    mvp day 3

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,9 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -31,32 +34,73 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+             return True
+        else:
+            if target < self.value:
+                if not self.left:
+                    return False
+                if self.left.value == target:
+                    return True
+                else:
+                    self.left.contains(target)
+            else: 
+                if not self.right:
+                    return False
+                if self.right.value == target:
+                    return True
+                else:
+                    self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if not self.right:
+             return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node is None:
+             return
+        self.in_order_print(node.left)
+        print(node.value)
+        self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        q = [node]
+        while True:
+            # print("---------")
+            val = q[0]
+            print(val.value)
+            q.remove(val)
+            if val.left:
+                q.append(val.left)
+            if val.right:
+                q.append(val.right)
+            if len(q) == 0: break
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        if node is None:
+             return
+        print(node.value)
+        self.dft_print(node.left)
+        self.dft_print(node.right)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
